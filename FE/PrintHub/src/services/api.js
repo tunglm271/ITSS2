cb73173405f59566dba2aaea2f80c67691fef966
hoangh20 <tranduchoangh20@gmail.com>
2025-05-18 22:57:52 +0700

    update api url

--- FE/PrintHub/src/services/api.js
+++ FE/PrintHub/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://itss-2-be-sigma.vercel.app";
+const API_URL = "https://faceted-flint-boat.glitch.me/";
 
 const api = axios.create({
     baseURL: API_URL,
